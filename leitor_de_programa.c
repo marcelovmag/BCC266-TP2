14b26a69e011d8c119429da4ddce39f06409296e
marcelovmag <81254324+marcelovmag@users.noreply.github.com>
2024-01-14 23:02:52 -0300

    finish leitor de programa, add comments

--- leitor_de_programa.c
+++ leitor_de_programa.c
@@ -1,11 +1,336 @@
 #include "leitor_de_programa.h"
+#include <string.h>
+#include <stdlib.h>
+
+#define MI leitor->minst
+#define linha leitor->linha
+#define n leitor->n
 
 void executaPrograma(Leitor* leitor) {
     
-    while (leitor->linha < leitor->n)
-    {   
+    char opc[4];
 
-        leitor->linha++;
+    while (linha < n)
+    {   
+        for (int i = 0; i < 3; i++)
+        {
+            opc[i] = MI[linha].comando[i];
+        }
+        opc[4] = '\0';
+
+        int i = 5;
+
+        if (!strcmp(opc, "ADD"))
+        {
+            char arg1[10], arg2[10]; // Os 2 argumentos necessarios para o opcode ADD
+            int i1 = 0, i2 = 0; // Indices para cada uma das strings "arg"
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg1[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg1[i1] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg2[i2] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i2++; // Incrementa o indice da string do argumento
+            }
+            arg2[i2] = '\0'; // Finaliza a string
+
+            int r1 = atoi(arg1);
+            int r2 = atoi(arg2);
+
+            add(leitor, r1, r2); // Executa a funcao relativa ao opcode com os devidos argumentos 
+        }
+
+        else if (!strcmp(opc, "SUB"))
+        {
+            char arg1[10], arg2[10]; // Os 2 argumentos necessarios para o opcode SUB
+            int i1 = 0, i2 = 0; // Indices para cada uma das strings "arg"
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg1[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg1[i1] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg2[i2] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i2++; // Incrementa o indice da string do argumento
+            }
+            arg2[i2] = '\0'; // Finaliza a string
+
+            int r1 = atoi(arg1);
+            int r2 = atoi(arg2);
+
+            sub(leitor, r1, r2); // Executa a funcao relativa ao opcode com os devidos argumentos 
+        }
+
+        else if (!strcmp(opc, "MUL"))
+        {
+            char arg1[10], arg2[10]; // Os 2 argumentos necessarios para o opcode MUL
+            int i1 = 0, i2 = 0; // Indices para cada uma das strings "arg"
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg1[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg1[i1] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg2[i2] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i2++; // Incrementa o indice da string do argumento
+            }
+            arg2[i2] = '\0'; // Finaliza a string
+
+            int r1 = atoi(arg1);
+            int r2 = atoi(arg2);
+
+            mul(leitor, r1, r2); // Executa a funcao relativa ao opcode com os devidos argumentos 
+        }
+
+        else if (!strcmp(opc, "DIV"))
+        {
+            char arg1[10], arg2[10]; // Os 2 argumentos necessarios para o opcode DIV
+            int i1 = 0, i2 = 0; // Indices para cada uma das strings "arg"
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg1[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg1[i1] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg2[i2] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i2++; // Incrementa o indice da string do argumento
+            }
+            arg2[i2] = '\0'; // Finaliza a string
+
+            int r1 = atoi(arg1);
+            int r2 = atoi(arg2);
+
+            div(leitor, r1, r2); // Executa a funcao relativa ao opcode com os devidos argumentos 
+        }
+
+        else if (!strcmp(opc, "REM"))
+        {
+            char arg1[10], arg2[10]; // Os 2 argumentos necessarios para o opcode REM
+            int i1 = 0, i2 = 0; // Indices para cada uma das strings "arg"
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg1[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg1[i1] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg2[i2] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i2++; // Incrementa o indice da string do argumento
+            }
+            arg2[i2] = '\0'; // Finaliza a string
+
+            int r1 = atoi(arg1);
+            int r2 = atoi(arg2);
+
+            rem(leitor, r1, r2); // Executa a funcao relativa ao opcode com os devidos argumentos 
+        }
+
+        else if (!strcmp(opc, "STO"))
+        {
+            char arg[10]; // O argumento necessario para o opcode STO
+            int i1 = 0; // Indice para a string arg
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg[i1] = '\0'; // Finaliza a string
+
+
+            int r1 = atoi(arg);
+
+            sto(leitor, r1); // Executa a funcao relativa ao opcode com os devidos argumentos 
+        }
+
+        else if (!strcmp(opc, "STI"))
+        {
+            char arg1[10], arg2[10]; // Os 2 argumentos necessarios para o opcode STI
+            int i1 = 0, i2 = 0; // Indices para cada uma das strings "arg"
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg1[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg1[i1] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg2[i2] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i2++; // Incrementa o indice da string do argumento
+            }
+            arg2[i2] = '\0'; // Finaliza a string
+
+            int r1 = atoi(arg1);
+            float valor = atof(arg2);
+
+            sti(leitor, r1, valor); // Executa a funcao relativa ao opcode com os devidos argumentos 
+        }
+
+        else if (!strcmp(opc, "BEQ"))
+        {
+            char arg1[10], arg2[10], arg3[10]; // Os 3 argumentos necessarios para o opcode BEQ
+            int i1 = 0, i2 = 0, i3 = 0; // Indices para cada uma das strings "arg"
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg1[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg1[i1] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg2[i2] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i2++; // Incrementa o indice da string do argumento
+            }
+            arg2[i2] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg3[i3] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i3++; // Incrementa o indice da string do argumento
+            }
+            arg3[i3] = '\0'; // Finaliza a string
+
+            int r1 = atoi(arg1);
+            int r2 = atoi(arg2);
+            int l = atoi(arg3);
+
+            beq(leitor, r1, r2, l); // Executa a funcao relativa ao opcode com os devidos argumentos
+        }
+
+        else if (!strcmp(opc, "BLT"))
+        {
+            char arg1[10], arg2[10], arg3[10]; // Os 3 argumentos necessarios para o opcode BLT
+            int i1 = 0, i2 = 0, i3 = 0; // Indices para cada uma das strings "arg"
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg1[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg1[i1] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg2[i2] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i2++; // Incrementa o indice da string do argumento
+            }
+            arg2[i2] = '\0'; // Finaliza a string
+            i++;
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            { 
+                arg3[i3] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i3++; // Incrementa o indice da string do argumento
+            }
+            arg3[i3] = '\0'; // Finaliza a string
+
+            int r1 = atoi(arg1);
+            int r2 = atoi(arg2);
+            int l = atoi(arg3);
+
+            blt(leitor, r1, r2, l); // Executa a funcao relativa ao opcode com os devidos argumentos
+        }
+
+        else if (!strcmp(opc, "JMP"))
+        {
+            char arg1[10]; // O argumento necessario para o opcode JMP
+            int i1 = 0; // Indice para a string arg
+
+            while (MI[linha].comando[i] != ' ' && MI[linha].comando[i] != '\n' && MI[linha].comando[i] != '\0') // testa o fim do argumento
+            {
+                arg1[i1] = MI[linha].comando[i];
+
+                i++; // Incrementa o indice da string do comando inteiro
+                i1++; // Incrementa o indice da string do argumento
+            }
+            arg1[i1] = '\0'; // Finaliza a string
+
+            int l = atoi(arg1);
+
+            jmp(leitor, l); // Executa a funcao relativa ao opcode com os devidos argumentos 
+        }
+
+        else if (!strcmp(opc, "DSP"))
+        {
+            dsp(leitor);
+        }
+
+        else
+            printf("\nComando inválido. Linha: %d. Comando: \"%s\"", linha, opc);
+        
+
+        linha++;
     }
     
     
--- memoria_principal.c
+++ memoria_principal.c
@@ -3,12 +3,12 @@
 void inicializa(MP* mp) {
     mp->memoria = fopen("memoria.dat", "wb+");
     mp->count = 0;
-}
+} // inicializa a memoria principal (abre o arquivo)
 
 void fecha(MP* mp) {
     fclose(mp->memoria);
     mp->count = 0;
-}
+} // encerra o acesso à memória principal (fecha o arquivo)
 
 int read(MP* mp, int i, int tam, float* output) {
     if(i >= 2048)
@@ -20,7 +20,7 @@ int read(MP* mp, int i, int tam, float* output) {
     fread(output, sizeof(float), tam, mp->memoria);
 
     return 1;
-}
+} // le o "i-esimo" float armazenado na memoria principal e retorna-os a partir do ponteiro output
 
 int write(MP* mp, int i, int tam, float* input) {
     if (mp->count + tam >= 2048)
@@ -34,4 +34,4 @@ int write(MP* mp, int i, int tam, float* input) {
 
     return 1;
     
-}
+} // escreve i floats armazenados a partir do ponteiro input
